5ce2e9351d93856e46ce671800bc48f5e88929b0
BestBlade <68151785+BestBlade@users.noreply.github.com>
2020-12-19 23:59:16 +0800

    Add files via upload

---
+++ leetcode_no98_验证二叉搜索树_w1.cpp
@@ -0,0 +1,74 @@
+﻿/* ------------------------------------------------------------------|
+给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+
+假设一个二叉搜索树具有如下特征：
+
+节点的左子树只包含小于当前节点的数。
+节点的右子树只包含大于当前节点的数。
+所有左子树和右子树自身必须也是二叉搜索树。
+示例 1:
+
+输入:
+    2
+   / \
+  1   3
+输出: true
+示例 2:
+
+输入:
+    5
+   / \
+  1   4
+     / \
+    3   6
+输出: false
+解释: 输入为: [5,1,4,null,null,3,6]。
+     根节点的值为 5 ，但是其右子节点值为 4 。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/validate-binary-search-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	双指针法
+*
+*	执行用时：0 ms, 在所有 C++ 提交中击败了100.00%的用户
+*	内存消耗：8.1 MB, 在所有 C++ 提交中击败了81.94%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+bool dfs(TreeNode* root, long long lower, long long upper) {
+    if (root == nullptr) {
+        return true;
+    }
+    if (root->val <= lower || root->val >= upper) {
+        return false;
+    }
+    return dfs(root->left, lower, root->val) && dfs(root->right, root->val, upper);
+}
+
+bool isValidBST(TreeNode* root) {
+    return dfs(root, LONG_MIN, LONG_MAX);
+}
